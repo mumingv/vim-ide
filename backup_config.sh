7d12f3296e56a34b6cbc22a454c85c9841e411fb
Jay Yin <mumingv@163.com>
2016-03-22 17:16:10 +0800

    add directory AuthorInfo

--- backup_config.sh
+++ backup_config.sh
@@ -9,6 +9,8 @@ cp -f ~/.vim/bundles.vim ./
 # 插件c.vim配置
 cp -rf ~/.vim/bundle/c.vim/c-support/codesnippets ./bundle/c.vim/c-support/
 cp -rf ~/.vim/bundle/c.vim/c-support/templates ./bundle/c.vim/c-support/
+# 插件AuthorInfo代码
+cp ~/.vim/bundle/AuthorInfo/plugin/authorinfo.vim bundle/AuthorInfo/plugin/authorinfo.vim
 
 echo -e "\e[32mBackup config files successfully. \e[0m"
 
--- deploy_config_step2.sh
+++ deploy_config_step2.sh
@@ -5,6 +5,8 @@
 # 恢复插件c.vim配置
 cp -rf ./bundle/c.vim/c-support/codesnippets ~/.vim/bundle/c.vim/c-support/
 cp -rf ./bundle/c.vim/c-support/templates ~/.vim/bundle/c.vim/c-support/
+# 恢复插件AuthorInfo代码
+cp bundle/AuthorInfo/plugin/authorinfo.vim ~/.vim/bundle/AuthorInfo/plugin/authorinfo.vim
 
 echo -e "\e[32mRestore config files successfully. \e[0m"
 
